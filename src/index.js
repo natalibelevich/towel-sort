9cc939a6c197c651c4295e8a800546427e36c711
Natallia Bialevich <natalibelevich94@gmail.com>
2020-03-23 00:20:20 +0300

    implement task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!Array.isArray(matrix)) {
+        return []
+    }
+    return matrix
+        .flatMap((arr, i) => i % 2 === 0 ?
+            arr.sort(Math.max) :
+            arr.sort(Math.max).reverse()
+        );
+};
